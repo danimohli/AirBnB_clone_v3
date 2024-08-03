9b1a35c72f476c608be366e999cfa99fc8ae12bd
danimohli <danimohlife@gmail.com>
2024-08-03 18:38:44 +0100

    index_endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,32 +15,38 @@ from models.amenity import Amenity
 from models.review import Review
 
 
-classes = {
-    "users": "User",
-    "places": "Place",
-    "states": "State",
-    "cities": "City",
-    "amenities": "Amenity",
-    "reviews": "Review"
-}
-
-
-@app_views.route('/status', methods=['GET'])
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
     """
     Route to check the status of the API
     Returns a JSON response with status 'OK'
     """
-    return jsonify({'status': 'OK'})
+    data = {
+        "status": "OK"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
 
 
-@app_views.route('/stats', methods=['GET'])
-def count():
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
     """
     Route to retrieve the number of each object type
     Returns a JSON response with the count of each object type
     """
-    count_dict = {}
-    for cls in classes:
-        count_dict[cls] = storage.count(classes[cls])
-    return jsonify(count_dict)
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
