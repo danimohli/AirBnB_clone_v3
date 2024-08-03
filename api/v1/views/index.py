1cb8c5b9b1cc498f5aa08d577b4ba4db46d626cc
danimohli <danimohlife@gmail.com>
2024-08-03 14:13:04 +0100

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,15 +1,50 @@
 #!/usr/bin/python3
 """
-api/v1/views/index.py
+Index module for API endpoints
+Defines routes for status and object counts
 """
 
-from flask import jsonify
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+"""
+Dictionary mapping class names to their
+string representations
+"""
+
+
+classes = {
+    "users": "User",
+    "places": "Place",
+    "states": "State",
+    "cities": "City",
+    "amenities": "Amenity",
+    "reviews": "Review"
+}
 
 
 @app_views.route('/status', methods=['GET'])
-def get_status():
+def status():
+    """Route to check the status of the API
+    Returns a JSON response with status 'OK'
     """
-    Return the status of the API
+    return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'])
+def count():
     """
-    return jsonify({"status": "OK"})
+    Route to retrieve the number of each object type
+    Returns a JSON response with the count of each object type
+    """
+    count_dict = {}
+    for cls in classes:
+        count_dict[cls] = storage.count(classes[cls])
+    return jsonify(count_dict)
