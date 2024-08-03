4eede77100d1f63e208c94ba4af746e8d73d17b5
danimohli <danimohlife@gmail.com>
2024-08-03 15:52:52 +0100

    init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,6 +4,9 @@ app_view => blueprint
 """
 from flask import Blueprint
 
+"""
+Create a Blueprint instance for the API views
+"""
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 if app_views is not None:
