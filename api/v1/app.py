19d4acee5952fc9c80fcc936ed991eeec5724db7
danimohli <danimohlife@gmail.com>
2024-08-05 21:15:39 +0100

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -32,6 +32,7 @@ def not_found(error):
     """
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+
 if __name__ == '__main__':
     if getenv("HBNB_API_HOST") is None:
         HBNB_API_HOST = '0.0.0.0'
